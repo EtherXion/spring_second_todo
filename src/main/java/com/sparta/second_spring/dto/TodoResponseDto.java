a30a7eb82967c0d6974f29e952999935d7e0ef78
EtherXion <etel0103@gmail.com>
2024-08-28 19:42:28 +0900

    timestamped

--- src/main/java/com/sparta/second_spring/dto/TodoResponseDto.java
+++ src/main/java/com/sparta/second_spring/dto/TodoResponseDto.java
@@ -3,6 +3,8 @@ package com.sparta.second_spring.dto;
 import com.sparta.second_spring.entity.Todo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class TodoResponseDto {
 
@@ -11,12 +13,18 @@ public class TodoResponseDto {
     private String todoTitle;
     private String todoContent;
 
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
 
     public TodoResponseDto(Todo todo) {
         this.id = todo.getId();
         this.userName = todo.getUsername();
         this.todoTitle = todo.getTodotitle();
         this.todoContent = todo.getTodocontent();
+
+        this.createdAt = todo.getCreatedAt();
+        this.modifiedAt = todo.getModifiedAt();
     }
 
 
